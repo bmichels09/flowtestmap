ead0d7848dda4971172c2972e651e0413d13364b
bmichels09 <80725149+bmichels09@users.noreply.github.com>
2023-11-01 12:16:19 -0500

    update default zoom to 8

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,5 +1,5 @@
 // Create the map object with center, zoom level and default layer.
-var map = L.map('map').setView([44, -89], 9);
+var map = L.map('map').setView([44, -89], 8);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
