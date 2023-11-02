23b6116fb3adf45ad048f2ae2892e1bedd20161b
bmichels09 <80725149+bmichels09@users.noreply.github.com>
2023-11-02 13:52:22 -0500

    update map style, update tooltip formatting

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,44 +1,45 @@
 // Create the map object with center, zoom level and default layer.
 var map = L.map('map').setView([44, -89], 8);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	accessToken: api_key
+	accessToken: api_key,
+	tileSize: 512,
+	zoomOffset: -1
 }).addTo(map);
 
 d3.csv("data/FlowTests.csv", function(data) {
     for (var i = 0; i < data.length; i++) {
         let test = data[i];
 		let tooltip = "";
-		if (test.Latitude !== undefined && test.Longitude !== undefined) {
+		if (test.Latitude !== "" && test.Longitude !== "") {
 			
-			if (test.Address !== undefined) {
-				tooltip = tooltip + test.Address + "<br>";
+			if (test.Address !== "") {
+				tooltip = tooltip + `<b>${test.Address}</b><br>`;
 			}
 
-			if (test.City !== undefined && test.State !== undefined) {
-				tooltip = tooltip + `${test.City}, ${test.State}<br>`;
+			if (test.City !== "" && test.State !== "") {
+				tooltip = tooltip + `<b>${test.City}, ${test.State}</b><br><br>`;
 			}
 
-			if (test.Date !== undefined) {
-				tooltip = tooltip + `Date: ${test.Date}<br>`;
+			if (test.Date !== "") {
+				tooltip = tooltip + `<b>Date:</b> ${test.Date}<br>`;
 			}
 
-			if (test.Source !== undefined) {
-				tooltip = tooltip + `Source: ${test.Source}<br>`;
+			if (test.Source !== "") {
+				tooltip = tooltip + `<b>Source:</b> ${test.Source}<br><br>`;
 			}
 
-			if (test.Static !== undefined) {
-				tooltip = tooltip + `Static: ${test.Static}<br>`;
+			if (test.Static !== "") {
+				tooltip = tooltip + `<b>Static:</b> ${test.Static} psi<br>`;
 			}
 
-			if (test.Residual !== undefined) {
-				tooltip = tooltip + `Residual: ${test.Residual}<br>`;
+			if (test.Residual !== "") {
+				tooltip = tooltip + `<b>Residual:</b> ${test.Residual} psi<br>`;
 			}
 
-			if (test.Flow !== undefined) {
-				tooltip = tooltip + `Flow: ${test.Flow}<br>`;
+			if (test.Flow !== "") {
+				tooltip = tooltip + `<b>Flow:</b> ${test.Flow} gpm<br>`;
 			}
 
 			tooltip = tooltip.trimEnd();
